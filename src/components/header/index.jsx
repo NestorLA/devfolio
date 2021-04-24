1d34a02f0ba3cdce7243d6290ce3cfbb8e769b90
Nestor Acerbo <nestoracerbo@gmail.com>
2021-04-23 23:17:40 -0300

    cambio de formato de imagen a webp, cambio de height para lighthouse

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import { Link } from 'gatsby';
 import get from 'lodash/get';
 import React from 'react';
 
-import profileImg from '../../images/yo2.jpg';
+import profileImg from '../../images/yo2.webp';
 
 const classes = {
   wrapper: 'block mb-6 md:flex',
