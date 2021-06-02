419719b69178515340dbfba54bec85d3b0aa5c35
Nestor Acerbo <nestoracerbo@gmail.com>
2021-06-02 20:46:49 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -149,35 +149,5 @@ module.exports = {
         icon: `src/images/icon.png`,
       },
     },
-    {
-      resolve: `gatsby-plugin-react-i18next`,
-      options: {
-        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
-        languages: [`es`, `en`],
-        defaultLanguage: `es`,
-        // if you are using Helmet, you must include siteUrl, and make sure you add http:https
-        siteUrl: `https://nestoracerbo.vercel.app/`,
-        // you can pass any i18next options
-        // pass following options to allow message content as a key
-        i18nextOptions: {
-          interpolation: {
-            escapeValue: false // not needed for react as it escapes by default
-          },
-          keySeparator: false,
-          nsSeparator: false
-        },
-        pages: [
-          {
-            matchPath: '/:lang?/blog/:uid',
-            getLanguageFromPath: true,
-            excludeLanguages: ['en']
-          },
-          {
-            matchPath: '/preview',
-            languages: ['es']
-          }
-        ]
-      }
-    }
   ],
 };
